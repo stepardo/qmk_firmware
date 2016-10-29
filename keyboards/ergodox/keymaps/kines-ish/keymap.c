326b11f8b5b47f8221f1a222b77d89a636be96b8
Steffen Liebergeld <perl@gmx.org>
2016-10-29 09:37:52 +0200

    Add mouse scroll keys

--- keyboards/ergodox/keymaps/kines-ish/keymap.c
+++ keyboards/ergodox/keymaps/kines-ish/keymap.c
@@ -42,9 +42,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                          KC_BSPC,KC_DEL,KC_END,
         // right hand
              KC_RGHT,     KC_6,   KC_7,   KC_8,   KC_9,   KC_0,     KC_MINS,
-             TG(SYMB),    KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,     KC_BSLS,
+             KC_WH_U,     KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,     KC_BSLS,
                           KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,  KC_QUOT,
-             MEH_T(KC_NO),KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,  KC_RSFT,
+             KC_WH_D,     KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,  KC_RSFT,
                                   KC_DOWN,  KC_UP,KC_LBRC,KC_RBRC,  TG(MDIA),
              KC_LALT, CTL_T(KC_ESC),
              KC_PGUP,
