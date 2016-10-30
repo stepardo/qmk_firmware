304e07e0f7dd0e7f54758305bc5982833f724bf4
Steffen Liebergeld <perl@gmx.org>
2016-10-30 21:32:51 +0100

    Have a macro to insert umlauts

--- keyboards/ergodox/keymaps/kines-ish/keymap.c
+++ keyboards/ergodox/keymaps/kines-ish/keymap.c
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
              KC_RGHT,     KC_6,   KC_7,   KC_8,   KC_9,   KC_0,     KC_MINS,
              KC_WH_U,     KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,     KC_BSLS,
                           KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,  KC_QUOT,
-             KC_WH_D,     KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,  KC_RSFT,
+             M(42),       KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,  KC_RSFT,
                                   KC_DOWN,  KC_UP,KC_LBRC,KC_RBRC,  TG(MDIA),
              KC_LALT, CTL_T(KC_ESC),
              KC_PGUP,
@@ -149,6 +149,11 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
         } else {
           unregister_code(KC_RSFT);
         }
+        break;
+        case 42:
+          if (record->event.pressed) {
+            return MACRO( T(CAPS), D(LSFT), T(QUOT), U(LSFT) );
+          }
         break;
       }
     return MACRO_NONE;
