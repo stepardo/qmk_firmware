d08f24d7f83a7be6b1e66e9de782d967af6e74a8
Steffen Liebergeld <perl@gmx.org>
2017-02-06 18:21:32 +0100

    latest

--- keyboards/ergodox/keymaps/kines-ish/keymap.c
+++ keyboards/ergodox/keymaps/kines-ish/keymap.c
@@ -35,16 +35,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_EQL,   KC_1,    KC_2,    KC_3,     KC_4,     KC_5,  KC_ESC,
         KC_TAB,   KC_Q,    KC_W,    KC_E,     KC_R,     KC_T,  TG(SYMB),
         KC_CAPS,  KC_A,    KC_S,    KC_D,     KC_F,     KC_G,
-        KC_LSFT,  KC_Z,    KC_X,    KC_C,     KC_V,     KC_B,  M(42),
+        KC_LSFT,  KC_Z,    KC_X,    KC_C,     KC_V,     KC_B,  MO(MDIA),
         TG(MDIA), KC_GRV,  M(42),   KC_LEFT,  KC_RGHT,
                                                CTL_T(KC_ESC),KC_LALT,
                                                        KC_HOME,
-                                         CTL_T(KC_BSPC), ALT_T(KC_DEL),KC_END,
+                                         CTL_T(KC_BSPC), ALT_T(KC_DEL), KC_END,
         // right hand
              KC_ESC,      KC_6,   KC_7,   KC_8,   KC_9,   KC_0,     KC_MINS,
              KC_BTN2,     KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,     KC_BSLS,
                           KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,  KC_QUOT,
-             KC_BTN3,     KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,  KC_RSFT,
+             KC_ESC,     KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,  KC_RSFT,
                                   KC_DOWN,  KC_UP,KC_LBRC,KC_RBRC,  TG(MDIA),
              KC_LALT, CTL_T(KC_ESC),
              KC_PGUP,
@@ -131,7 +131,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                           KC_VOLU, KC_VOLD, KC_MUTE, KC_TRNS, KC_TRNS,
        KC_TRNS, KC_TRNS,
        KC_TRNS,
-       KC_TRNS, KC_TRNS, KC_WBAK
+       KC_BTN3, KC_BTN2, KC_BTN1
 ),
 };
 
