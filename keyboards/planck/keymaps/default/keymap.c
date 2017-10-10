89f4b7f3ae2bae658a08161be4e79385d34150da
Steffen Liebergeld <perl@gmx.org>
2017-10-10 22:49:23 +0200

    latest changes

--- keyboards/planck/keymaps/default/keymap.c
+++ keyboards/planck/keymaps/default/keymap.c
@@ -25,6 +25,8 @@ enum planck_layers {
   _DVORAK,
   _MOUSE,
   _DIRECTIONS,
+  _PAGEUP_DOWN,
+  _BRACKETS,
   _LOWER,
   _RAISE,
   _PLOVER,
@@ -56,17 +58,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = {
-  {KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS},
-  {KC_ESC,  KC_A,    KC_S,    KC_FN1,  KC_FN0,  KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT},
-  {KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT },
+  {KC_TAB,  KC_Q,    KC_W,    KC_FN2,  KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS},
+  {KC_ESC,  KC_A,    KC_S,    KC_FN1,  KC_FN0,  KC_G,    KC_H,    KC_FN3,  KC_K,    KC_L,    KC_SCLN, KC_QUOT},
+  {KC_LSPO, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC },
   {KC_NO,   KC_LCTL, KC_LALT, KC_LGUI, LOWER,   ALT_T(KC_BSPC),   CTL_T(KC_SPC),  RAISE,   ALGR_T(KC_LEFT), KC_DOWN, KC_UP,   KC_RGHT}
 },
 
 [_MOUSE] = {
-  {KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R,  KC_TRNS, KC_TRNS},
-  {KC_ESC,  KC_A,    KC_S,    KC_D,    KC_TRNS, KC_G,    KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R,  KC_SCLN, KC_TRNS},
-  {KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_TRNS},
-  {KC_NO,   KC_LCTL, KC_LALT, KC_LGUI, KC_WWW_BACK,   KC_BTN1, KC_BTN2,    KC_WWW_FORWARD,   KC_LEFT, KC_DOWN,  KC_UP,   KC_TRNS}
+  {KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_WH_L, KC_WH_D,        KC_WH_U, KC_WH_R,  KC_TRNS, KC_TRNS},
+  {KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_MS_L, KC_MS_D,        KC_MS_U, KC_MS_R,  KC_TRNS, KC_TRNS},
+  {KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS,        KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS},
+  {KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_WWW_BACK, KC_BTN1, KC_BTN2, KC_WWW_FORWARD, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS}
 },
 
 [_DIRECTIONS] = {
@@ -74,6 +76,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_TRNS, KC_MUTE},
   {KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_VOLU},
   {KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_VOLD}
+},
+
+[_PAGEUP_DOWN] = {
+  {KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS, KC_MPRV,    KC_MNXT, KC_MPLY},
+  {KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, S(KC_PGDN), KC_PGDN, KC_PGUP, S(KC_PGUP), KC_TRNS, KC_MUTE},
+  {KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS, KC_VOLU},
+  {KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS, KC_VOLD}
+},
+
+[_BRACKETS] = {
+  {KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LPRN, KC_RPRN, KC_TRNS,    KC_TRNS, KC_TRNS, KC_MPRV,    KC_MNXT, KC_MPLY},
+  {KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LCBR, KC_RCBR, KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS, KC_MUTE},
+  {KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LBRC, KC_RBRC, KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS, KC_VOLU},
+  {KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS, KC_VOLD}
 },
 
 /* Colemak
@@ -126,8 +142,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_LOWER] = {
   {KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,    KC_ASTR,    KC_LPRN, KC_RPRN, KC_TRNS},
   {KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS,    KC_PLUS,    KC_LCBR, KC_RCBR, KC_PIPE},
-  {_______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  S(KC_NUHS), S(KC_NUBS), KC_HOME, KC_END,  _______},
-  {_______, _______, _______, _______, _______, _______, KC_ENT,  _______,    KC_VOLD,    KC_VOLU, KC_MNXT, KC_MPLY}
+  {KC_HOME, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  S(KC_NUHS), S(KC_NUBS), _______, _______, KC_END},
+  {_______, _______, _______, _______, _______, _______, KC_ENT,  _______,    _______,    _______, _______, _______}
 },
 
 /* Raise
@@ -144,8 +160,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_RAISE] = {
   {KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_TRNS},
   {KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS},
-  {_______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NUHS, KC_NUBS, KC_PGUP, KC_PGDN, _______},
-  {_______, _______, _______, _______, _______, KC_DEL,  _______, _______, KC_VOLD, KC_VOLU, KC_MPRV, KC_MPLY}
+  {_______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NUHS, KC_NUBS, KC_PGDN, KC_PGUP, _______},
+  {_______, _______, _______, _______, _______, KC_DEL,  _______, _______, _______, _______, _______, _______}
 },
 
 /* Plover layer (http://opensteno.org)
@@ -194,8 +210,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 #endif
 
 const uint16_t PROGMEM fn_actions[] = {
-  [0] = ACTION_LAYER_TAP_KEY(_MOUSE, KC_F), // momentarily switch to layer if
+  [0] = ACTION_LAYER_TAP_KEY(_MOUSE, KC_F),      // momentarily switch to layer if
   [1] = ACTION_LAYER_TAP_KEY(_DIRECTIONS, KC_D), // pressed long pressed long
+  [2] = ACTION_LAYER_TAP_KEY(_PAGEUP_DOWN, KC_E),
+  [3] = ACTION_LAYER_TAP_KEY(_BRACKETS, KC_J),
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
