6a54949d152f9e954641fe395550b92e0c9ab117
Steffen Liebergeld <perl@gmx.org>
2018-07-16 13:44:21 +0200

    stepardo_ez: remove MDIA layer, add multimedia to DIRECTIONS layer

--- layouts/community/ergodox/stepardo_ez/keymap.c
+++ layouts/community/ergodox/stepardo_ez/keymap.c
@@ -41,7 +41,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [BASE] = LAYOUT_ergodox(  // layer 0 : default
         // left hand
         KC_EQL,   KC_1,    KC_2,    KC_3,     KC_4,     KC_5,  KC_ESC,
-        KC_TAB,   KC_Q,    KC_W,    KC_E,     KC_R,     KC_T,  KC_NO,
+        KC_TAB,   KC_Q,    KC_W,    KC_E,     KC_R,     KC_T,  KC_WWW_BACK,
         KC_ESC,   KC_A,    KC_S,    KC_FN1,   KC_FN0,   KC_G,
         KC_LSFT,  KC_Z,    KC_X,    KC_C,     KC_V,     KC_B,  EXWM_OPEN,
         KC_ESC,   KC_GRV,  KC_INS,  KC_LEFT,  KC_RGHT,
@@ -52,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
              KC_ESC,         KC_6,   KC_7,   KC_8,   KC_9,   KC_0,     KC_MINS,
              KC_WWW_FORWARD, KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,     KC_BSLS,
                              KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,  KC_QUOT,
-             KC_WWW_BACK,    KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,  KC_RSFT,
+             KC_NO,          KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,  KC_RSFT,
                                      KC_DOWN,KC_UP,  KC_LBRC,KC_RBRC,  KC_NO,
              KC_LGUI, KC_RCTL,
              KC_PGUP,
@@ -100,47 +100,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_TRNS,
        KC_TRNS, KC_TRNS, KC_TRNS
 ),
-/* Keymap 2: Media and mouse keys
- *
- * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |        |      |      |      |      |      |      |           |      |      |      |      |      |      |        |
- * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * |        |      |      | MsUp |      |      |      |           |      |      |      |      |      |      |        |
- * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        |      |MsLeft|MsDown|MsRght|      |------|           |------|      |      |      |      |      |  Play  |
- * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        |      |      |      |      |      |      |           |      |      |      | Prev | Next |      |        |
- * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |      |      |      | Lclk | Rclk |                                       |VolUp |VolDn | Mute |      |      |
- *   `----------------------------------'                                       `----------------------------------'
- *                                        ,-------------.       ,-------------.
- *                                        |      |      |       |      |      |
- *                                 ,------|------|------|       |------+------+------.
- *                                 |      |      |      |       |      |      |Brwser|
- *                                 |      |      |------|       |------|      |Back  |
- *                                 |      |      |      |       |      |      |      |
- *                                 `--------------------'       `--------------------'
- */
-// MEDIA AND MOUSE
-[MDIA] = LAYOUT_ergodox(
-       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-       KC_TRNS, KC_TRNS, KC_TRNS, KC_MS_U, KC_TRNS, KC_TRNS, KC_TRNS,
-       KC_TRNS, KC_TRNS, KC_MS_L, KC_MS_D, KC_MS_R, KC_TRNS,
-       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-       KC_TRNS, KC_TRNS, KC_TRNS, KC_BTN1, KC_BTN2,
-                                           KC_TRNS, KC_TRNS,
-                                                    KC_TRNS,
-                                  KC_TRNS, KC_TRNS, KC_TRNS,
-    // right hand
-       KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-       KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-                 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MPLY,
-       KC_TRNS,  KC_TRNS, KC_TRNS, KC_MPRV, KC_MNXT, KC_TRNS, KC_TRNS,
-                          KC_VOLU, KC_VOLD, KC_MUTE, KC_TRNS, KC_TRNS,
-       KC_TRNS, KC_TRNS,
-       KC_TRNS,
-       KC_TRNS, KC_TRNS, KC_WBAK
-),
 [DIRECTIONS] = LAYOUT_ergodox(  // directions layer
         // left hand
         KC_TRNS,   KC_TRNS,    KC_TRNS,    KC_TRNS,     KC_TRNS,     KC_TRNS,  KC_TRNS,
@@ -152,10 +111,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                                                KC_TRNS,
                                                         KC_TRNS,     KC_TRNS,  KC_TRNS,
         // right hand
-        KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,     KC_TRNS,
-        KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,     KC_TRNS,
+        KC_MUTE,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_MPRV,   KC_MNXT,     KC_MPLY,
+        KC_VOLU,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,     KC_TRNS,
                    KC_LEFT,   KC_DOWN,   KC_UP,     KC_RIGHT,  KC_TRNS,     KC_TRNS,
-        KC_TRNS,   KC_TRNS,   KC_PGDN,   KC_PGUP,   KC_TRNS,   KC_TRNS,     KC_TRNS,
+        KC_VOLD,   KC_TRNS,   KC_PGDN,   KC_PGUP,   KC_TRNS,   KC_TRNS,     KC_TRNS,
                               KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,     KC_TRNS,
         KC_TRNS,   KC_TRNS,
         KC_TRNS,
